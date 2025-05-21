5e449801dae7dd85e336c55246a9f7348ec92502
Bozo Guy <izykiguy@gmail.com>
2025-05-21 10:23:05 +0200

    'The first comit'

---
+++ TODOO-app/TODOO-app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TODOO_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TODOO-app/TODOO-app/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TODOO_app
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Items = new ObservableCollection<string>();
+            this.BindingContext = this;
+        }
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(AddListItem.Text))
+            {
+                Items.Add(AddListItem.Text);
+                AddListItem.Text = string.Empty;
+            }
+        }
+
+        private async void DeleteOnClick(object sender, EventArgs e)
+        {
+            if (sender is Button button && button.BindingContext is string item)
+            {
+                bool answer = await DisplayAlert("Confirm Delete", $"Are you sure you want to delete '{item}'?", "Yes", "No");
+                if (answer)
+                {
+                    Items.Remove(item);
+                }
+            }
+        }
+    }
+}
